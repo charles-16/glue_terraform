b1a45b7155a60bc33a1f1b2808dccc3bf0f1ef38
charles-16 <39376312+charles-16@users.noreply.github.com>
2024-11-22 20:46:18 +0530

    Update main.tf

--- s3_tgt/main.tf
+++ s3_tgt/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "Glue-test-s3" {
-   bucket = "glue-cj-dest11-s3"
+   bucket = "glue-cj-dest13-s3"
    tags = {
      Name           = "cj-buck"
      Environment    = "dev"
