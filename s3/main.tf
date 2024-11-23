9944ce7b68ed7cfeccff7f852366fe491c6ed3ff
charles-16 <39376312+charles-16@users.noreply.github.com>
2024-11-23 16:45:51 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "Glue-test-s3" {
-   bucket = "my-temp-bucket"
+   bucket = "my-temp-bucket-cj17"
    tags = {
      Name           = "my-temp-bucket"
      Environment    = "dev"
