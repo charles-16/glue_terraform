37fb78836d39d92ae8079b204adad5d87ba3be2e
charles-16 <39376312+charles-16@users.noreply.github.com>
2024-11-23 16:11:51 +0530

    added an folder

--- s3_tgt/main.tf
+++ s3_tgt/main.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "Glue-test-s3" {
      Environment    = "dev"
    }
 }
+
+# Create a folder within the S3 bucket
+resource "aws_s3_bucket_object" "transformed_folder" {
+  bucket = aws_s3_bucket.Glue-test-s3.bucket
+  key    = "transformed/"  # The trailing slash indicates a folder
+}
